7815f2fffb4be68780c811ae239c510cf2b385e1
Josh Kim <joshuak94@zedat.fu-berlin.de>
2021-04-14 18:08:56 +0200

    Update iterator test to test for file positions.

--- test/unit/io/detail/in_file_iterator_test.cpp
+++ test/unit/io/detail/in_file_iterator_test.cpp
@@ -7,45 +7,64 @@
 
 #include <gtest/gtest.h>
 
-#include <vector>
+#include <iostream>
+#include <memory>
 
 #include <seqan3/std/iterator>
 
 #include <seqan3/io/detail/in_file_iterator.hpp>
+#include <seqan3/io/detail/misc_input.hpp>
 
 //NOTE(h-2): This class is extensively tested via *_file_input. This is just a minimal test.
 
-struct fake_file_t : std::vector<int>
+struct fake_file_t : std::basic_istream<char>
 {
-    using base = std::vector<int>;
+    using base = std::basic_istream<char>;
 
     using base::base;
 
     using iterator = seqan3::detail::in_file_iterator<fake_file_t>;
+    using value_type = char;
+    using reference = const char&;
+    using size_type = size_t;
+    using difference_type = std::ptrdiff_t;
+    using stream_ptr_t = std::unique_ptr<std::basic_istream<char>,
+                                         std::function<void(std::basic_istream<char>*)>>;
 
-    size_t current_position = 0;
-    bool at_end = false;
+    bool at_end{false};
+    char record_buffer{};
+    stream_ptr_t secondary_stream{};
+    stream_ptr_t primary_stream{};
+    std::streampos position_buffer{};
 
     void read_next_record()
     {
-        ++current_position;
-        if (current_position == size())
+        position_buffer = secondary_stream->tellg();
+
+        // at end if we could not read further
+        if (secondary_stream->eof())
+        {
             at_end = true;
-        else
-            record_buffer = base::operator[](current_position);
+            return;
+        }
+
+        secondary_stream->get(record_buffer);
     }
 
     iterator begin()
     {
-        record_buffer = base::operator[](current_position);
+        secondary_stream->seekg(0);
+        this->read_next_record();
         return {*this};
     }
 
-    int record_buffer;
+    static void stream_deleter_default(std::basic_istream<char> * ptr) { delete ptr; }
 
-    fake_file_t() :
-        base{}
-    {}
+    fake_file_t(std::istringstream in) :
+        primary_stream{new std::istringstream{std::move(in)}, stream_deleter_default}
+    {
+        secondary_stream = seqan3::detail::make_secondary_istream(*primary_stream);
+    }
 };
 
 TEST(in_file_iterator, concepts)
@@ -59,11 +78,11 @@ TEST(in_file_iterator, member_types)
 {
     using it_t = seqan3::detail::in_file_iterator<fake_file_t>;
     EXPECT_TRUE((std::is_same_v<typename it_t::value_type,
-                                int>));
+                                char>));
     EXPECT_TRUE((std::is_same_v<typename it_t::reference,
-                                int &>));
+                                const char &>));
     EXPECT_TRUE((std::is_same_v<typename it_t::const_reference,
-                                int &>));
+                                const char &>));
     EXPECT_TRUE((std::is_same_v<typename it_t::difference_type,
                                 std::ptrdiff_t>));
     EXPECT_TRUE((std::is_same_v<typename it_t::size_type,
@@ -76,37 +95,59 @@ TEST(in_file_iterator, operations)
 {
     using it_t = seqan3::detail::in_file_iterator<fake_file_t>;
 
-    fake_file_t f{1, 2, 3, 4, 5, 6, 8};
+    fake_file_t f{std::istringstream{"helloworld"}};
 
     // construct
     it_t it = f.begin();
 
     // deref
-    EXPECT_EQ(*it, 1);
+    EXPECT_EQ(*it, 'h');
 
     // pre-inc
-    EXPECT_EQ(*(++it), 2);
+    EXPECT_EQ(*(++it), 'e');
 
     // post-inc
     it++;
 
     // deref
-    EXPECT_EQ(*it, 3);
+    EXPECT_EQ(*it, 'l');
 }
 
 TEST(in_file_iterator, comparison)
 {
     using it_t = seqan3::detail::in_file_iterator<fake_file_t>;
 
-    fake_file_t f{1, 2, 3, 4, 5, 6, 8};
+    fake_file_t f{std::istringstream{"helloworld"}};
     it_t it = f.begin();
 
     // not at end
     EXPECT_FALSE(it == std::default_sentinel);
 
     // consume the entire range
-    ++it; ++it; ++it; ++it; ++it; ++it; ++it;
+    ++it; ++it; ++it; ++it; ++it; ++it; ++it; ++it; ++it; ++it; ++it;
 
     // at end
     EXPECT_TRUE(it == std::default_sentinel);
+}
+
+TEST(in_file_iterator, file_position)
+{
+    using it_t = seqan3::detail::in_file_iterator<fake_file_t>;
+
+    fake_file_t f{std::istringstream{"helloworld"}};
+    it_t it = f.begin();
+    auto beginning = it.file_position();
+
+    // Go to the 6th character (w) and store it.
+    ++it; ++it; ++it; ++it; ++it;
+    EXPECT_EQ(*it, 'w');
+    auto w_position = it.file_position();
+
+    // Go back to the beginning.
+    it.seek_to(beginning);
+    EXPECT_EQ(*it, 'h');
+
+    // Go directly to the w.
+    it.seek_to(w_position);
+    EXPECT_EQ(*it, 'w');
 }
